00971e27c8d1a922ac340df09eb54a96b3b78c1f
johanneshoelker <johannes.hoelker@uni-kassel.de>
2023-10-11 09:43:48 +0200

    added controlConsumer.h

---
+++ src/consumercontrol/controlConsumer.h
@@ -0,0 +1,9 @@
+// using FLD_PDC for the Power DC value
+// using RadioHead library for 433MHz sender
+
+//logic:
+
+//if FLD_PDC is higher than THRESHOLD_ON
+//  send command for switching socket adaptor on
+//if FLD_PDC is lower than THRESHOLD_OFF
+//  send command for switching socket adaptor off
